462dba7755443e038651d220e3072b71ccda7fe6
Chuse <chasevillarroel@gmail.com>
2023-08-24 18:20:10 +0800

    pipe client factory

--- utils/named_pipes.lua
+++ utils/named_pipes.lua
@@ -33,7 +33,7 @@ function M.create_server(opts)
 		json = function(client, chunk)
 			local success, _ = pcall(vim.json.decode, chunk)
 			if not success then
-				client:write(opts.default or "Invalid request: Not in JSON format")
+				client:write(opts.default or "Invalid request: Not in JSON format\n")
 				return false
 			end
 			return true
@@ -57,6 +57,70 @@ function M.create_server(opts)
 				if not success then
 					client:write(opts.default or ("Internal server error: " .. err .. "\n"))
 				end
+			else
+				client:shutdown()
+				client:close()
+			end
+		end)
+	end)
+end
+
+---@class client_opts
+---@field pipe_name string
+---@field callback callback
+---@field format? formats
+---@field default? string
+
+---@param opts client_opts
+function M.create_client(opts)
+	---@type formats
+	local format = opts.format or "string"
+
+	function assert(val, err)
+		if val then
+			return val
+		end
+		if opts.default then
+			io.write(opts.default)
+			os.exit(0)
+		else
+			io.write(err .. "\n")
+			os.exit(1)
+		end
+	end
+
+	---@type uv_pipe_t
+	local client = assert(uv.new_pipe(false))
+	local pipe_path = "\\\\.\\pipe\\" .. opts.pipe_name
+
+	---@type table<formats, callback>
+	local verify_format = {
+		string = function(_, _)
+			return true
+		end,
+
+		json = function(_, chunk)
+			local success, _ = pcall(vim.json.decode, chunk)
+			assert(success, "Server error: Invalid reply (not in JSON format):\n" .. chunk)
+			return true
+		end,
+	}
+
+	client:connect(pipe_path, function(err)
+		assert(not err, "Error connecting to server: " .. err)
+		client:read_start(function(err, chunk)
+			assert(not err, "Error connecting to server: " .. err)
+			if chunk then
+				if not verify_format[format](client, chunk) then
+					return
+				end
+
+				local success, err = pcall(opts.callback, client, chunk)
+				assert(success, "Client error: " .. err)
+
+				client:shutdown()
+				client:close()
+				os.exit(0)
 			else
 				client:shutdown()
 				client:close()
